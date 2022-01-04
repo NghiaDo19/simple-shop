60f366ff7bd77724471f5df91b00c8f31e92922e
Nghia Do <nghia.do@tiki.vn>
2022-01-04 11:58:02 +0700

    add structure and tailwind css

---
+++ components/MyAccount/index.js
@@ -0,0 +1,24 @@
+import { memo } from 'react';
+// import { useHeading } from 'components/CartPageV2/MainProduct/Heading/hooks';
+
+import { StyledMyAccount, SideBar } from './styles';
+
+const MyAccount = memo(() => {
+
+  return (
+    <StyledMyAccount>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <SideBar>
+        <li>Giao Diện</li>
+        <li>Thống Kê</li>
+        <li>Thông Tin Cửa Hàng</li>
+        <li>Thêm/Sửa Sản Phẩm</li>
+        <li>Đơn Hàng</li>
+      </SideBar>
+    </StyledMyAccount>
+  );
+});
+
+export default MyAccount;
---
+++ components/MyAccount/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+export const StyledMyAccount = styled.div`
+  
+`;
+
+export const SideBar = styled.div`
+  
+`;
